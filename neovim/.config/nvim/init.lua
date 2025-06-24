6121aba5c5080a693bb7c0ee5278385bc99f1084
Jack Trowbridge <jytrowbridge@gmail.com>
2025-06-24 09:47:34 -0400

    initial commit

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,19 @@
+require("config.lazy")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.number = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', {clear = true}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+print "hello world"
